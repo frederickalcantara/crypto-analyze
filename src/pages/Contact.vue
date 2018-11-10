fe78c535fa150aca6b54f6c957c0172a6a752b53
frederickalcantara <frederickaalcantara@gmail.com>
2018-11-09 19:45:48 -0500

    Adding more form config

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,28 +2,28 @@
   <div>
     <Header />
     <div class="container">
-    <h1 class="title">
-      Contact
-    </h1>
-    <div class="content">
-      <form name="contact" action="" method="post" netlify>
-        <input type="hidden" name="contact" value="contact">
-        <label class="form-label" for="name">
-          Name:
-        </label>
-        <input class="form-field" name="name" id="name" />
-        <label class="form-label" for="email">
-          Email:
-        </label>
-        <input class="form-field" name="email" id="email" />
-        <label class="form-label" for="message">
-          Message:
-        </label>
-        <textarea class="form-field" name="message" id="message"></textarea>
-        <input class="form-button" type="submit" value="Send message" />
-      </form>
+      <h1 class="title">
+        Contact
+      </h1>
+      <div class="content">
+        <form name="contact" action="" method="post" netlify>
+          <input type="hidden" name="contact" value="contact">
+          <label class="form-label" for="name">
+            Name:
+          </label>
+          <input class="form-field" name="name" id="name" />
+          <label class="form-label" for="email">
+            Email:
+          </label>
+          <input class="form-field" name="email" id="email" />
+          <label class="form-label" for="message">
+            Message:
+          </label>
+          <textarea class="form-field" name="message" id="message"></textarea>
+          <input class="form-button" type="submit" value="Send message" />
+        </form>
+      </div>
     </div>
-  </div>
   </div>
 </template>
 
